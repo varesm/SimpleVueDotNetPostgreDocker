2ca0eac826b80af15225a82153259003f4554c79
varesm <e.varasteh@gmail.com>
2023-03-31 12:32:27 +0300

    initial commit

---
+++ FirstDocker.API/FirstprjContext.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace FirstDocker.API;
+
+public partial class FirstprjContext : DbContext
+{
+    public FirstprjContext()
+    {
+    }
+
+    public FirstprjContext(DbContextOptions<FirstprjContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<User> Users { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseNpgsql("Host=localhost:5432;Database=firstprj;Username=postgres;Password=1qaz@WSX");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<User>(entity =>
+        {
+            entity.HasKey(e => e.Id).HasName("users_pkey");
+
+            entity.ToTable("users");
+
+            entity.Property(e => e.Id).UseIdentityAlwaysColumn();
+            entity.Property(e => e.Emailaddress)
+                .HasMaxLength(100)
+                .HasColumnName("emailaddress");
+            entity.Property(e => e.Firstname)
+                .HasMaxLength(50)
+                .HasColumnName("firstname");
+            entity.Property(e => e.Lastname)
+                .HasMaxLength(50)
+                .HasColumnName("lastname");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ FirstDocker.API/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FirstDocker.API;
+
+public partial class User
+{
+    public string? Firstname { get; set; }
+
+    public string? Lastname { get; set; }
+
+    public int Id { get; set; }
+
+    public string? Emailaddress { get; set; }
+}
